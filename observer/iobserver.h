1e6e81f57b162cc408ab98433585fed173fc3f38
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-06-13 21:30:23 +0530

    Update IObserver

---
+++ observer/iobserver.h
@@ -0,0 +1,17 @@
+#ifndef FOOTBALL_GAME_ENGINE_IOBSERVER_H
+#define FOOTBALL_GAME_ENGINE_IOBSERVER_H
+
+/*
+ * IObserver Class
+ * This is an abstract base class for the entire observer
+ * pattern, with a pure virtual method for updating the
+ * child classes.
+ */
+
+class IObserver
+{
+public:
+    virtual void update() const = 0;
+};
+
+#endif
